faee6769a02eb116da1c3058b518f54d5ca572c9
Ash4947 <124432161+Ash4947@users.noreply.github.com>
2023-10-22 18:28:36 +0530

    Create examplecontract.sol

---
+++ examplecontract.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.0;
+
+contract Accreditation {
+    struct Course {
+        string name;
+        uint credits;
+        bool accredited;
+    }
+    //Maps address of course with course details and course accredtitation with student address
+    mapping(address => mapping(string => Course)) public studentCourses;
+    mapping(address => uint) public studentTotalCredits;
+
+    function accreditCourse(
+        address student,
+        string memory courseName,
+        uint courseCredits
+    ) public {
+        // Implement access control logic here to ensure only accrediting authorities can call this function.
+        
+        require(!studentCourses[student][courseName].accredited, "Course already accredited");
+        
+        studentCourses[student][courseName] = Course(courseName, courseCredits, true);
+        studentTotalCredits[student] += courseCredits;
+    }
+    
+    function getStudentCourse(address student, string memory courseName) public view returns (string memory, uint, bool) {
+        Course memory course = studentCourses[student][courseName];
+        return (course.name, course.credits, course.accredited);
+    }
+}
